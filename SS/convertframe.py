82085d951eed0f580b33f335a74b49fd7a89c03c
antoniusbayu76 <antoniusbayu96@gmail.com>
2024-03-18 22:06:04 +0700

    Update convertframe.py

--- SS/convertframe.py
+++ SS/convertframe.py
@@ -5,23 +5,23 @@ cap = cv2.VideoCapture(path)
 
 # upper = np.array([128, 192, 194])
 # lower = np.array([43, 9, 61])
-count = 3
+count = 1
 
 while True:
     ##Ambil frame video
     status, img = cap.read()
 
-    frame = cv2.GaussianBlur(img, (21,21), cv2.BORDER_DEFAULT)
-    # img_name ="img{}.jpg".format(count)
-    # cv2.imwrite(img_name,img)
-    # count += 1
+    # frame = cv2.GaussianBlur(img, (21,21), cv2.BORDER_DEFAULT)
+    img_name ="img{}.jpg".format(count)
+    cv2.imwrite(img_name,img)
+    count += 1
 
-    k = cv2.waitKey(1)
+    # k = cv2.waitKey(1)
 
-    if k%256 == 32:
-        img_name ="img{}.png".format(count)
-        cv2.imwrite(img_name,img)
-        count =+ 1
+    # if k%256 == 32:
+    #     img_name ="img{}.png".format(count)
+    #     cv2.imwrite(img_name,img)
+    #     count =+ 1
 
     cv2.imshow('raw image', img)
 
